5a84dd1d721a6b6132c615f822c19fa0b6ad231d
thayrov <thayrovg@gmail.com>
2023-09-20 23:50:23 -0600

    complete theory for M06

---
+++ M06 JavaScript Objetos/Theory/objects.js
@@ -0,0 +1,69 @@
+var deportes = {
+	conBalon: ['Futbol', 'Basketball', 'Golf'],
+	sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+};
+var persona = {nombre: 'Lucas', edad: 26, estudios: {esProgramador: true}};
+
+// ACCEDER
+var persona = {nombre: 'Lucas', edad: 26, estudios: {esProgramador: true}};
+console.log(persona.edad);
+
+// ASIGNAR
+var persona = {nombre: 'Lucas', edad: 26, estudios: {esProgramador: true}};
+persona.nombre = 'Martín';
+console.log(persona.nombre);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+
+//BORRAR
+delete autos.marcas;
+console.log(autos);
+
+// DOT NOTATION
+var atuendos = {manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes']};
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+console.log(atuendos);
+
+var comidas = {};
+var diferenciaDeNotaciones = function (propUno, propDos) {
+	comidas.propUno = ['Frutas', 'Vegetales'];
+	comidas[propDos] = ['Hamburguesa', 'Papas Fritas'];
+};
+diferenciaDeNotaciones('saludable', 'noSaludable');
+console.log(comidas);
+
+// HAS OWN PROPERTY
+var libro = {autor: 'Borges', genero: 'Policial', año: 1990};
+var tienePropiedad = libro.hasOwnProperty('nombre');
+
+console.log(tienePropiedad);
+
+// KEYS
+var libro = {autor: 'Borges', genero: 'Policial', año: 1990};
+var todasLasPropiedades = Object.keys(libro);
+
+console.log(todasLasPropiedades);
+
+// FOR IN
+var mundo = {continentes: 7, paises: 195, oceanos: 5};
+
+for (let prop in mundo) {
+	console.log('Esta es la propiedad: ', prop);
+	console.log('Este es el valor: ', mundo[prop]);
+}
+
+//THIS
+var mascota = {
+	animal: 'Perro',
+	raza: 'Ovejero Alemán',
+	amistoso: true,
+	dueño: 'María López',
+	info: () => {
+		console.log('Mi perro es un  ' + this.raza);
+	},
+};
